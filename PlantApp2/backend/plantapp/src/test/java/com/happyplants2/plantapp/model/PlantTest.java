c4b4bc36c5cada2ad4c36a73dd6743fe06ae4a3e
LilasBe <lilasberakdar1995@gmail.com>
2026-02-09 09:58:07 +0100

    Create PlantTest.java

---
+++ PlantApp2/backend/plantapp/src/test/java/com/happyplants2/plantapp/model/PlantTest.java
@@ -0,0 +1,36 @@
+package com.happyplants2.plantapp.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlantTest {
+
+    @Test
+    void ProgressNeverBelowMinimum() {
+        Plant plant = new Plant("Rose","1", LocalDate.now().minusDays(100), 5,"img" );
+        assertEquals(0.02, plant.getProgress());
+    }
+
+    @Test
+    void ProgressReturnsFullWhenRecentlyWatered(){
+        Plant plant = new Plant("Rose","1", LocalDate.now(), 5,"img" );
+        assertEquals(1, plant.getProgress());
+    }
+
+    @Test
+    void plantsNeedsWateringWhenDaysPassed() {
+        Plant plant = new Plant("Rose","1", LocalDate.now().minusDays(5), 5,"img" );
+        assertTrue(plant.needsWatering());
+    }
+
+    @Test
+    void wateringUpdatesLastWateredDay() {
+        Plant plant = new Plant("Rose","1", LocalDate.now().minusDays(5), 5,"img" );
+        plant.water();
+        assertEquals(LocalDate.now(), plant.getLastWatered());
+    }
+}
