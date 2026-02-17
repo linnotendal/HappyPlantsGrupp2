5ba971de24dcff0b6cc7818c41bbeab0dc56bc14
Bjornheimer <christoffer.bjornheimer@gmail.com>
2026-02-17 10:28:04 +0100

    Test for github actions

--- PlantApp2/backend/plantapp/src/main/java/com/happyplants2/plantapp/PlantappApplication.java
+++ PlantApp2/backend/plantapp/src/main/java/com/happyplants2/plantapp/PlantappApplication.java
@@ -31,5 +31,5 @@ public class PlantappApplication {
                 System.out.println("Added 3 test plants");
             }
         };
-    } //testar min github actionsbranch
+    } //TODO test för github actions
 }
