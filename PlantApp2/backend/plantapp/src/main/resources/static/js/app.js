f8fbc348e754ce41afd015c5f194a0aa24240f83
LilasBe <lilasberakdar1995@gmail.com>
2026-02-25 16:01:40 +0100

    Cleaned up unused classes, Tested creating user and adding plants related to a specific user

--- PlantApp2/backend/plantapp/src/main/resources/static/js/app.js
+++ PlantApp2/backend/plantapp/src/main/resources/static/js/app.js
@@ -23,6 +23,7 @@ function showSection(sectionName) {
 
 async function renderDiscoverSection(plants = null) {
     const grid = document.getElementById('discover-grid');
+    const emptyMsg = document.getElementById('empty-library-msg');
     grid.innerHTML = '<p>Loading...</p>';
 
     try {
@@ -110,10 +111,30 @@ async function handleAddFromDiscover(event, plantData) {
 async function renderLibrarySection(filteredPlants = null) {
     const grid = document.getElementById('library-grid');
     const emptyMsg = document.getElementById('empty-library-msg');
+    const sortSelect = document.getElementById('sort-library-select');
     if (filteredPlants==null){
         grid.innerHTML = '<p>Loading...</p>';
     }
     const library = filteredPlants || await api.getLibrary();
+
+    if (library && library.length > 0) {
+        const sortBy = sortSelect.value;
+
+        if (sortBy === 'water-status') {
+            library.sort((a, b) => {
+                const progressA = typeof a.getProgress === 'function' ? a.getProgress() : 0;
+                const progressB = typeof b.getProgress === 'function' ? b.getProgress() : 0;
+                return progressA - progressB;
+            });
+        } else {
+            library.sort((a, b) => {
+                const nameA = a.name || "";
+                const nameB = b.name || "";
+                return nameA.localeCompare(nameB);
+            });
+        }
+    }
+
     grid.innerHTML = '';
 
     if (library.length === 0) {
